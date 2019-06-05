d01506c1872f0c76531608a07565631c6c428d49
Jorge Olmeda <jorge.olmeda@gmail.com>
2019-06-05 19:21:35 +0200

    initial version

---
+++ Frontend/clientes-app/src/app/clientes/clientes.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import {Cliente} from './cliente';
+import {ClienteService} from './cliente.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html'
+})
+export class ClientesComponent implements OnInit {
+
+  clientes: Cliente[];
+
+  constructor(private ClienteService: ClienteService) { }
+
+  ngOnInit() {
+    this.ClienteService.getClientes().subscribe(
+      //es una funcion anonima donde el parametro de entrada es clientes function(){}
+      clientes => this.clientes = clientes
+    );
+  }
+
+  delete(cliente: Cliente): void {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false,
+    })
+
+    swalWithBootstrapButtons.fire({
+      title: '¿Está seguro?',
+      text: `¿Seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, eliminar!',
+      cancelButtonText: 'No, cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+
+        this.ClienteService.delete(cliente.id).subscribe(
+          response => {
+            this.clientes = this.clientes.filter(cli => cli !== cliente)
+            swalWithBootstrapButtons.fire(
+              'Cliente eliminado!',
+              `CLiente ${cliente.nombre} eliminado con éxito.`,
+              'success'
+            )
+          }
+        )
+
+      }
+    })
+  }
+}
